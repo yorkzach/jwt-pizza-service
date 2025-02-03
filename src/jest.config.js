32d44c3926e082067e2b79c4fcd0a00ffd47a6ab
“Zach <zacharyyork0@gmail.com>
2025-02-03 10:35:51 -0700

    debugging jest.config.js

---
+++ src/jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+    verbose: true,
+    collectCoverage: true,
+    coverageReporters: ["json-summary", "text"],
+    coverageDirectory: 'coverage',
+  };
+  
+module.exports = config;
