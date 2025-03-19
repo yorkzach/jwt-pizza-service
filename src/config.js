6516dad3c1455e6f5b0541dade6d8e0536cff361
“Zach <zacharyyork0@gmail.com>
2025-03-18 18:35:29 -0600

    updated config.js and CI.yml

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
-module.exports = {
+module.exports =  {
   jwtSecret: 'zacharyyork',
   db: {
     connection: {
       host: '127.0.0.1',
+      // host: 'host.docker.internal',
       user: 'root',
-      password: 'tempdbpassword',
+      password: 'C1rcle11',
       database: 'pizza',
       connectTimeout: 60000,
     },
@@ -13,5 +14,10 @@ module.exports = {
   factory: {
     url: 'https://pizza-factory.cs329.click',
     apiKey: 'f8e76ea7567d465cb8c5e0c70da31cd7',
+  },
+  metrics: { 
+    source: 'jwt-pizza-service-dev',
+    url: 'https://otlp-gateway-prod-us-west-0.grafana.net/otlp/v1/metrics',
+    apiKey: '1203969:glc_eyJvIjoiMTM4MDA1MCIsIm4iOiJzdGFjay0xMjAzOTY5LWludGVncmF0aW9uLWp3dC1waXp6YS1tZXRyaWNzIiwiayI6IjhJbzh6MWc1VnZsNFdyRDd2VDNiNzA3WCIsIm0iOnsiciI6InByb2QtdXMtd2VzdC0wIn19',
   },
 };
