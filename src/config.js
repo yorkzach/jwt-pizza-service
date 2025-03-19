f71b70f1560438a1336b8163031b1246de0ba1d6
Zachary York <126907716+yorkzach@users.noreply.github.com>
2025-03-18 18:41:15 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -14,4 +14,9 @@ module.exports = {
     url: 'https://pizza-factory.cs329.click',
     apiKey: 'f8e76ea7567d465cb8c5e0c70da31cd7',
   },
+  metrics: { 
+    source: 'jwt-pizza-service-dev',
+    url: 'https://otlp-gateway-prod-us-west-0.grafana.net/otlp/v1/metrics',
+    apiKey: '1203969:glc_eyJvIjoiMTM4MDA1MCIsIm4iOiJzdGFjay0xMjAzOTY5LWludGVncmF0aW9uLWp3dC1waXp6YS1tZXRyaWNzIiwiayI6IjhJbzh6MWc1VnZsNFdyRDd2VDNiNzA3WCIsIm0iOnsiciI6InByb2QtdXMtd2VzdC0wIn19',
+  },
 };
